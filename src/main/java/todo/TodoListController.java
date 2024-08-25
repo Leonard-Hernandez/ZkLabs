7a408a6816145acf9d3746c1fb3757aaa84c2864
Leonard <Leonardhernandez02@gmail.com>
2024-08-24 22:29:55 -0500

    añadiendo el update del crud

--- src/main/java/todo/TodoListController.java
+++ src/main/java/todo/TodoListController.java
@@ -2,39 +2,126 @@ package todo;
 
 import java.util.List;
 
+import org.zkoss.lang.Strings;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.util.Clients;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.Checkbox;
+import org.zkoss.zul.Datebox;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Radiogroup;
+import org.zkoss.zul.Textbox;
 
 import entity.Priority;
 import entity.Todo;
 import services.TodoListService;
 
-public class TodoListController extends SelectorComposer<Component>{
-	
+public class TodoListController extends SelectorComposer<Component> {
+
+	@Wire
+	Textbox todoSubject;
+	@Wire
+	Button addTodo;
 	@Wire
 	Listbox todoListBox;
-	
-	
-	//service
+
+	@Wire
+	Component selectedTodoBlock;
+	@Wire
+	Checkbox selectedTodoCheck;
+	@Wire
+	Textbox selectedTodoSubject;
+	@Wire
+	Radiogroup selectedTodoPriority;
+	@Wire
+	Datebox selectedTodoDate;
+	@Wire
+	Textbox selectedTodoDescription;
+	@Wire
+	Button updateSelectedTodo;
+
+	// service
 	TodoListService todoListService = new TodoListServiceImpl();
-	
-	//datos para la vista
+
+	// datos para la vista
 	ListModelList<Todo> todoListModel;
 	ListModelList<Priority> priorityListModel;
 	Todo selectedTodo;
-	
+
 	@Override
 	public void doAfterCompose(Component comp) throws Exception {
 		super.doAfterCompose(comp);
-		
+
 		List<Todo> todoList = todoListService.getTodoList();
 		todoListModel = new ListModelList<Todo>(todoList);
 		todoListBox.setModel(todoListModel);
-		
+
+		priorityListModel = new ListModelList<Priority>(Priority.values());
+		selectedTodoPriority.setModel(priorityListModel);
+
 	}
-	
-	
+
+	@Listen("onClick =#addTodo; onOK = #todoSubject")
+	public void soTodoAdd() {
+		// obteniendo el input del usuario
+		String subject = todoSubject.getValue();
+		if (Strings.isBlank(subject)) {
+			Clients.showNotification("Nada por hacer ?", todoSubject);
+		} else {
+			// guardando datos
+			selectedTodo = todoListService.saveTodo(new Todo(subject));
+			// actualizando el modelo
+			todoListModel.add(selectedTodo);
+			// asignando la nueva seleccion
+			todoListModel.addToSelection(selectedTodo);
+
+			// refresh detail view
+			refreshDetailView();
+
+			// reiniciando el valor
+			todoSubject.setValue("");
+		}
+	}
+
+	@Listen("onSelect = #todoListbox")
+	public void doTodoSelect() {
+		if (todoListModel.isSelectionEmpty()) {
+			selectedTodo = null;
+		}else {
+			selectedTodo = todoListModel.getSelection().iterator().next();
+		}
+		refreshDetailView();
+	}
+
+	private void refreshDetailView() {
+		// refresh the detail view of selected todo
+
+		if (selectedTodo == null) {
+			// clean
+			selectedTodoBlock.setVisible(false);
+			selectedTodoCheck.setChecked(false);
+			selectedTodoSubject.setValue(null);
+			selectedTodoDate.setValue(null);
+			selectedTodoDescription.setValue(null);
+			updateSelectedTodo.setDisabled(true);
+
+			priorityListModel.clearSelection();
+		} else {
+			selectedTodoBlock.setVisible(true);
+			selectedTodoCheck.setChecked(selectedTodo.isComplete());
+			selectedTodoSubject.setValue(selectedTodo.getSubject());
+			selectedTodoDate.setValue(selectedTodo.getDate());
+			selectedTodoDescription.setValue(selectedTodo.getDescription());
+			updateSelectedTodo.setDisabled(false);
+
+			priorityListModel.addToSelection(selectedTodo.getPriority());
+
+		}
+
+	}
+
 }
